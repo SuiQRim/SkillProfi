9ff61b6493557d5ae2ee3fb62ef3684bad092266
Roman Samadi <romikkimor123@gmail.com>
2022-09-05 01:08:28 +0300

    Добавьте файлы проекта.

---
+++ SkillProfiModels/Consultation.cs
@@ -0,0 +1,15 @@
+﻿namespace SkillProfi
+{
+    public class Consultation
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string EMail { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime Created { get; set; }
+    }
+}
