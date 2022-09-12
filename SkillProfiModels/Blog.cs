c3167cfe7ddf690c16eb0524ce667e6e44ac0fa4
Roman Samadi <romikkimor123@gmail.com>
2022-09-13 02:05:49 +0300

    Added a picture name generator.
    Update - Picture format be defined as rarely as possible
    
    Fix - now picture byte present not mapped in db
    Tags [required] [mapped] removed from interfaces and added in implementing classes

--- SkillProfiModels/Blog.cs
+++ SkillProfiModels/Blog.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,7 @@ namespace SkillProfi
         [Required]
         public string PictureName { get; set; }
 
+        [NotMapped]
         public byte[]? PictureBytePresentation { get; set; }
 
         public DateTime Created { get; set; }
