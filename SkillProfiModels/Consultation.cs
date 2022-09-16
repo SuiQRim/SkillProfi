1e68072919f7ceea8af9d7ec43d4eef1081a1893
Roman Samadi <romikkimor123@gmail.com>
2022-09-16 19:36:54 +0300

    Added Cors service with full resolution for cross-domain requests. Update Contultation Post method

--- SkillProfiModels/Consultation.cs
+++ SkillProfiModels/Consultation.cs
@@ -1,16 +1,21 @@
-﻿namespace SkillProfi
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SkillProfi
 {
     public class Consultation
     {
         public Guid Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public string EMail { get; set; }
 
+        [Required]
         public string Description { get; set; }
 
-        public string Status { get; set; }
+        public string? Status { get; set; }
 
         public DateTime Created { get; set; }
     }
