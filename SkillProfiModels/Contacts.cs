1a2ad1fac578584d83d023500ebb04f86f5d97e6
Roman Samadi <romikkimor123@gmail.com>
2022-09-29 02:41:23 +0300

    Link to the map is taken from the request Parameters are set by the page

--- SkillProfiModels/Contacts.cs
+++ SkillProfiModels/Contacts.cs
@@ -12,6 +12,9 @@ namespace SkillProfi
         public string? Adress { get; set; }
         public string? PhoneNumber { get; set; }
         public string? Email { get; set; }
+
+        public string? LinkToMapContructor { get; set; }
+
         public List<SocialNetwork>? SocialNetworks { get; set; }
 
     }
